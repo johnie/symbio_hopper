56c42274b70dfd6e8e5aac2859abfe956b782ded
Johnie Hjelm <johnie.hjelm@symbio.com>
2016-04-16 12:56:38 +0200

    Added a small customizer script

---
+++ customizer.js
@@ -0,0 +1,11 @@
+/**
+ * Symbio Hopper
+ * customizer.js
+ *
+ * copyright © 2015 - Johnie Hjelm <johnie.hjelm@symbio.com>
+ */
+
+// Change default favicon
+var icon = document.querySelector('link[rel="shortcut icon"]');
+
+icon.href = 'http://i.imgur.com/b5xPkrH.png';
